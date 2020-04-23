82bdf3c80d82ae1158269d74097c04d012e272c9
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-23 15:58:23 +0200

    forse finiti

---
+++ jsnack6/main.js
@@ -0,0 +1,8 @@
+//JSnack 6: Stampa le potenze di 2 fino a 1000
+for (var i = 0; i <= 1000; i++) {
+    var potenza = i;
+    var numero = 2;
+
+    var risultato = Math.pow(numero, potenza);
+    console.log(risultato);
+}
